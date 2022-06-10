b67ed56f31e4e110bd85e729b714f9463a630eaa
Kristaps <lidacisk@gmail.com>
2022-06-10 09:58:21 +0300

    First commit and push to arrays exercises

---
+++ basics/arrays/1.php
@@ -0,0 +1,28 @@
+<?php
+$numbers = [
+    1789, 2035, 1899, 1456, 2013,
+    1458, 2458, 1254, 1472, 2365,
+    1456, 2165, 1457, 2456
+];
+
+
+echo "Original numeric array: ";
+
+
+
+echo "Sorted numeric array: " ;
+
+$words = [
+    "Java",
+    "Python",
+    "PHP",
+    "C#",
+    "C Programming",
+    "C++"
+];
+
+
+echo "Original string array: ";
+
+
+echo "Sorted string array: ";
