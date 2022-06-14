b611e4d1883e2bb9092e5fa635d63dc2a19526b0
Kristaps <lidacisk@gmail.com>
2022-06-14 15:53:07 +0300

    Redone 05-functions 7th task - gunStore

--- basics/05-functions/7.php
+++ basics/05-functions/7.php
@@ -1,14 +1,14 @@
 <?php
 $person = new stdClass();
 $person->name = 'Kristaps';
-$person->license = 'mf';
+$person->license = ['B', 'A', 'B1'];
 $person->money = 1000;
 
 $store=[
-    addGun('mf', 123.25, 'Deagle'),
-    addGun('tv', 23.25, 'Airgun'),
-    addGun('tv', 883.25, 'Swifter'),
-    addGun('bk', 123.25, 'ShortBow')
+    addGun('A', 123.25, 'Deagle'),
+    addGun('A', 23.25, 'Airgun'),
+    addGun('C', 883.25, 'Swifter'),
+    addGun('B2', 123.25, 'ShortBow')
 
 ];
 
@@ -20,7 +20,17 @@ function addGun(string $license, float $price, string $name): stdClass{
     return $gun;
 }
 
-var_dump(in_array([$person->license], $store));
-
-
+foreach ($store as $item) {
+    if(in_array($item->license, $person->license)){
+        if($item->price < $person->money){
+            echo $person->name .' can buy: '. $item->name .' for '.$item->price .' with ' . ($person->money -
+                    $item->price) ." change".PHP_EOL;
+        }else{
+            echo $person->name .' can\'t buy: '. $item->name .' for '.$item->price.', because he has less money'.PHP_EOL;
+        }
+    }
+    else{
+        echo $person->name .' can\'t buy: '. $item->name .' because he don\'t have the license ' .$item->license .PHP_EOL;
+    }
+}
 
